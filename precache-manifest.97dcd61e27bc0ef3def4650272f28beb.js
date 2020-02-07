b319f00b01ab18e5285650c4dba6423a809cac50
Miguel Angel Civico <mcellinika@gmail.com>
2020-02-07 12:47:42 +0100

    .

---
+++ precache-manifest.97dcd61e27bc0ef3def4650272f28beb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edfedfd0ae2ace1515ab",
+    "url": "css/app.8caa578c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "a6bdbc511bdefdc26e359458d5420bbc",
+    "url": "index.html"
+  },
+  {
+    "revision": "edfedfd0ae2ace1515ab",
+    "url": "js/app.6cd8784f.js"
+  },
+  {
+    "revision": "083f825bd7b63ea74c70",
+    "url": "js/chunk-vendors.5006c987.js"
+  },
+  {
+    "revision": "06e633d84686d1a6e23063a451125d0c",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
